eb7ff3cec262df8e20866c503c3672a65b674397
nrasel <nrasel452@gmail.com>
2023-03-07 23:31:39 +0600

    before and update and create check the user is admin or not

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,13 +5,14 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/ProductController");
+const { isAdmin, authMiddleWare } = require("../middlewares/authMiddleware");
 
 const router = require("express").Router();
 
-router.post("/", createProduct);
+router.post("/", authMiddleWare, isAdmin, createProduct);
 router.get("/:id", getaProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.put("/:id", authMiddleWare, isAdmin, updateProduct);
+router.delete("/:id", authMiddleWare, isAdmin, deleteProduct);
 router.get("/", getallProducts);
 
 module.exports = router;
